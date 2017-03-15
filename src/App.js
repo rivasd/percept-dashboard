7f3792e471fbabba5d3838368997dedb1562c85e
Daniel Rivas <rivasdaniel1992@gmail.com>
2017-03-15 13:20:00 -0400

    renaming for npm start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+CodeMirror = require('react-codemirror');
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+
 
 class App extends Component {
   render() {
